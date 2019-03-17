5a21ae4bf1f6beb17b8986f6e30b69c752213acd
Alton Lu <lu.alton@gmail.com>
2019-03-17 12:04:22 -0400

    more timeseries

--- python/time-series.py
+++ python/time-series.py
@@ -115,3 +115,94 @@ ax = flights.plot(kind='density', linewidth=4, fontsize=6)
 ax.set_xlabel('Passengers', fontsize=10)
 ax.set_ylabel('Number of Passengers', fontsize=10)
 plt.show()
+
+
+#### Seasonality
+# Autocorrelation plot
+fig = tsaplots.plot_acf(flights['passengers'], lags=24)
+
+# Partial Autocorrelation
+fig = tsaplots.plot_pacf(flights['passengers'], lags=24)
+
+
+#### Time Series Decomposition
+import statsmodels.api as sm
+from pylab import rcParams
+decomposition = sm.tsa.seasonal_decompose(flights)
+rcParams['figure.figsize'] = 11, 9
+fig = decomposition.plot()
+plt.show()
+
+# Plot Individual Decomposed components
+trend = decomposition.trend
+ax = trend.plot(figsize=(12, 6), fontsize=6)
+plt.show()
+
+
+
+# Mutiple Time Series
+# Modifying some of the passenger data
+flights['kids'] = round(flights['passengers']/50) + random.randint(0, 2)
+flights['baggage'] = flights['passengers'] - random.randint(10, 100)
+
+# multiple time series plot
+flights.plot()
+
+# time series area plot
+flights.plot.area()
+
+# summary stats to plots
+flights_mean = pd.DataFrame(flights.mean()).transpose()
+flights_mean = flights_mean.rename(index={0:'mean'})
+
+ax = flights.plot(fontsize=6, linewidth=1)
+
+# Add x-axis labels
+ax.set_xlabel('Date', fontsize=6)
+
+# Add summary table information to the plot
+ax.table(cellText=flights_mean.values,
+         colWidths = [0.15]*len(flights_mean.columns),
+         rowLabels=flights_mean.index,
+         colLabels=flights_mean.columns,
+         loc='top')
+
+
+#### Subplots for multiple time series
+# How to create multiple plots so scaling issues are minimized
+flights.plot(subplots=True,
+          layout=(3,1),
+          sharex=False,
+          sharey=False,
+          colormap='viridis',
+          fontsize=10,
+          legend=True,
+          linewidth=0.5)
+
+plt.show()
+
+
+#### Correlations for time-series
+
+flight_corr = flights.corr()
+flight_corr
+
+# Making a heatmat
+sns.heatmap(flight_corr,
+            annot=True,
+            linewidths=0.4,
+            annot_kws={"size": 10})
+plt.xticks(rotation=90)
+plt.yticks(rotation=0)
+plt.show()
+
+
+# Making a cluster heatmat
+fig = sns.clustermap(flight_corr,
+                     row_cluster=True,
+                     col_cluster=True,
+                     figsize=(10, 10))
+
+plt.setp(fig.ax_heatmap.xaxis.get_majorticklabels(), rotation=90)
+plt.setp(fig.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
+plt.show()
